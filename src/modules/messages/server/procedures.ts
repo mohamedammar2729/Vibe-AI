c8e28c8ec5c31a6ccc4c2808615660a1e3c5564b
mohamedammar2729 <mo.bas.ammar@el-eng.menofia.edu.eg>
2025-07-23 14:22:21 +0300

    08: messages

---
+++ src/modules/messages/server/procedures.ts
@@ -0,0 +1,43 @@
+import { inngest } from '@/inngest/client';
+import { prisma } from '@/lib/db';
+import { baseProcedure, createTRPCRouter } from '@/trpc/init';
+import { z } from 'zod';
+
+export const messagesRouter = createTRPCRouter({
+  // Define your procedures here
+  create: baseProcedure
+    .input(
+      z.object({
+        value: z.string().min(1, { message: 'Message is required' }),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const createMessage = await prisma.message.create({
+        data: {
+          content: input.value,
+          role: 'USER', // Assuming the role is 'user' for this example
+          type: 'RESULT', // Replace with actual conversation ID logic
+        },
+      });
+
+      await inngest.send({
+        name: 'code-agent/run',
+        data: {
+          value: input.value,
+        },
+      });
+      return createMessage;
+    }),
+
+    getMany: baseProcedure.query(async () => {
+      const messages = await prisma.message.findMany({
+        orderBy: {
+          createdAt: 'desc',
+        },
+        // include: {
+        //   Fragment: true, // Include related fragment data if needed
+        // },
+      });
+      return messages;
+    }),
+});
